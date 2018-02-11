5bc3e518d67074886a8afecfcd822dec2cd89957
JuS <you@example.com>
2018-02-11 19:30:29 +0100

    Letzte Anpassungen vor finaler Version

--- build/lede/files/lib/gluon/web/controller/admin/index.lua
+++ build/lede/files/lib/gluon/web/controller/admin/index.lua
@@ -2,5 +2,5 @@ local e=node()
 if not e.target then
 e.target=alias("admin")
 end
-entry({"admin"},alias("admin","upgrade"),_("Advanced settings"),10)
-entry({"admin","info"},template("admin/info"),_("Information"),1)
+entry({"admin"},alias("admin","upgrade"),_("Advanced settings"),1)
+entry({"admin","info"},template("admin/info"),_("Information"),100)
