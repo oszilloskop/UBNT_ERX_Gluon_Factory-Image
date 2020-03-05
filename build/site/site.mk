89d855094d13a4d6899a5ae5f8a3f174a9fc24a5
JuS <sagurna@s3i.de>
2020-03-05 22:50:27 +0100

    Erste Anpassungen an Gluon 2020.1

--- build/site/site.mk
+++ build/site/site.mk
@@ -1,17 +1,19 @@
 GLUON_SITE_PACKAGES := \
-        gluon-setup-mode \
-        gluon-config-mode-core \
         gluon-config-mode-autoupdater \
-        gluon-web-admin \
-        gluon-status-page \
         haveged \
         -opkg \
 
-#####################################################################################################################
+
+##################################################################################################################$
 
 # Gluon Base Release
-DEFAULT_GLUON_RELEASE := GluonFactoryImage-v1.0
+DEFAULT_GLUON_RELEASE := GluonFactoryImage-v2.0
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
+# Languages to include.
+GLUON_LANGS ?= de en
+
+# Do not build images for deprecated devices
+GLUON_DEPRECATED ?= 0
